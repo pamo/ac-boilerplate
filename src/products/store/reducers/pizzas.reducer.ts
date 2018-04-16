7ce81c2230dcca9363ac10888a87fe18b371a545
Pamela Ocampo <pamela.ocampo@gmail.com>
2018-04-16 14:53:41 -0600

    First pizza reducer

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,8 +1,23 @@
 import * as fromPizzas from '../actions/pizzas.action';
 import { Pizza } from '../../models/pizza.model';
 
-export interface PizzaState {}
+export interface PizzaState {
+  loaded: boolean;
+  loading: boolean;
+  pizzas: Pizza[];
+}
 
-const initialState: PizzaState = {};
+const initialState: PizzaState = {
+  loaded: false,
+  loading: false,
+  pizzas: [],
+};
 
-export function reducer() {}
+export function reducer(state = initialState, action: fromPizzas.PizzasAction): PizzaState {
+  switch (action.type) {
+    case fromPizzas.LOAD_PIZZAS: {
+      return { ...state, loading: true, loaded: false };
+    }
+  }
+  return state;
+}
