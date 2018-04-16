f7d10402d9c631ce4c21a2922dc74fe86cffa914
Pamela Ocampo <pamela.ocampo@gmail.com>
2018-04-16 14:53:25 -0600

    Initial pizza actions

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -3,11 +3,28 @@ import { Action } from '@ngrx/store';
 import { Pizza } from '../../models/pizza.model';
 
 export const MY_ACTION = '[Feature] My Action';
+export const LOAD_PIZZAS = '[Pizza] Load Pizzas';
+export const LOAD_PIZZAS_FAIL = '[Pizza] Load Pizzas Fail';
+export const LOAD_PIZZAS_SUCCESS = '[Pizza] Load Pizzas Success';
 
 export class MyAction implements Action {
   readonly type = MY_ACTION;
   constructor(public payload: any) {}
 }
 
+export class LoadPizzas implements Action {
+  readonly type = LOAD_PIZZAS;
+  constructor() {}
+}
+
+export class LoadPizzasFail implements Action {
+  readonly type = MY_ACTION;
+  constructor(public payload: any) {}
+}
+export class LoadPizzasSuccess implements Action {
+  readonly type = MY_ACTION;
+  constructor(public payload: Pizza[]) {}
+}
+
 // action types
-export type PizzasAction = MyAction;
+export type PizzasAction = MyAction | LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
